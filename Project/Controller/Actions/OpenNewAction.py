7e696899254ef8a0de5fb6c98a61f721624d845a
andrei-lucian <lucianandrei12@gmail.com>
2021-05-15 15:53:15 +0200

    save option pops up only if table was modified

--- Project/Controller/Actions/OpenNewAction.py
+++ Project/Controller/Actions/OpenNewAction.py
@@ -6,31 +6,29 @@ def uponActionPerformed(mainWindow, qtw):
 # opens a dialog with option to save, then clears the table window
 
 def openNewAction(mainWindow, qtw):
-    # saveOption = qtw.QMessageBox.question(mainWindow, 'Save',
-    #                                       'Would you like to save before creating a new spreadsheet?',
-    #                                       qtw.QMessageBox.Yes, qtw.QMessageBox.No)
 
-    msgBox = qtw.QMessageBox(mainWindow)
-    msgBox.setText("The document has been modified.")
-    msgBox.setInformativeText("Do you want to save your changes?")
-    msgBox.setWindowTitle("Save")
-    msgBox.setStandardButtons(qtw.QMessageBox.Yes | qtw.QMessageBox.Discard | qtw.QMessageBox.Cancel)
-    saveOption = msgBox.exec_()
+    #check if data is empty before we give the option to save
+    data = mainWindow.tableView.backEnd.getData()
 
-    if saveOption == qtw.QMessageBox.Yes:
-        options = qtw.QFileDialog.Options()
-        filePath, _ = qtw.QFileDialog.getOpenFileName(mainWindow, "QFileDialog.getOpenFileNames()",
-                                                      "", "Table files (*.csv)", options=options)
+    if not data.empty:
+        msgBox = qtw.QMessageBox(mainWindow)
+        msgBox.setText("The document has been modified.")
+        msgBox.setInformativeText("Do you want to save your changes?")
+        msgBox.setWindowTitle("Save")
+        msgBox.setStandardButtons(qtw.QMessageBox.Yes | qtw.QMessageBox.Discard | qtw.QMessageBox.Cancel)
+        saveOption = msgBox.exec_()
 
-        # If files are selected, send  these to the InputHandler
-        if filePath:
-            mainWindow.setWindowTitle(filePath)
-            parseCSVFiles(filePath, mainWindow)
+        if saveOption == qtw.QMessageBox.Yes:
+            options = qtw.QFileDialog.Options()
+            filePath, _ = qtw.QFileDialog.getOpenFileName(mainWindow, "QFileDialog.getOpenFileNames()",
+                                                          "", "Table files (*.csv)", options=options)
+
+            # If files are selected, send  these to the InputHandler
+            if filePath:
+                mainWindow.setWindowTitle(filePath)
+                parseCSVFiles(filePath, mainWindow)
+                mainWindow.tableView.backEnd.clear()
+
+        elif saveOption == qtw.QMessageBox.Discard:
+            mainWindow.setWindowTitle("Untitled")
             mainWindow.tableView.backEnd.clear()
-
-    elif saveOption == qtw.QMessageBox.Discard:
-        mainWindow.setWindowTitle("Untitled")
-        mainWindow.tableView.backEnd.clear()
-
-
-

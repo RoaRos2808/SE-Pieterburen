2ac7f69c1aacade82a7aad3850227712e28e7476
mikelucas1 <m.lucas.1@student.rug.nl>
2021-05-20 16:21:37 +0200

    ADDED: readded open shortcut

--- Project/Controller/Buttons/OpenButton.py
+++ Project/Controller/Buttons/OpenButton.py
@@ -3,5 +3,6 @@ from Project.Controller.Actions import OpenAction
 
 def openButton(mainWindow, qtw):
     mainWindow.OpenButton = qtw.QAction('Open', mainWindow, checkable=False)
+    mainWindow.OpenButton.setShortcut('Ctrl+o')
     mainWindow.OpenButton.triggered.connect(lambda: OpenAction.uponActionPerformed(mainWindow, qtw))
 

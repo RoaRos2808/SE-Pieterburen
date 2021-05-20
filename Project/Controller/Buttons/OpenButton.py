e4bd49081191d233c32b9a6d81c4a8b48b9e21e8
mikelucas1 <m.lucas.1@student.rug.nl>
2021-05-20 16:29:07 +0200

    changed: progressbar updates in increments of 1

--- Project/Controller/Buttons/OpenButton.py
+++ Project/Controller/Buttons/OpenButton.py
@@ -5,4 +5,3 @@ def openButton(mainWindow, qtw):
     mainWindow.OpenButton = qtw.QAction('Open', mainWindow, checkable=False)
     mainWindow.OpenButton.setShortcut('Ctrl+o')
     mainWindow.OpenButton.triggered.connect(lambda: OpenAction.uponActionPerformed(mainWindow, qtw))
-

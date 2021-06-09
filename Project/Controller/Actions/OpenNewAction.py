eeac4db70d228191b7cd020f59874241be76e10c
Brian <b.de.jager.4@student.rug.nl>
2021-06-09 14:38:14 +0200

    BUG: OpenNewAction now also removes empty columns

--- Project/Controller/Actions/OpenNewAction.py
+++ Project/Controller/Actions/OpenNewAction.py
@@ -8,38 +8,34 @@ def uponActionPerformed(mainWindow, qtw):
 
 # opens a dialog with option to save, then clears the table window
 def openNewAction(mainWindow, qtw):
-    # check if data is empty before we give the option to save
-    data = mainWindow.tableView.backEnd.getData()
+    be = mainWindow.getBackEnd()
+    if mainWindow.windowTitle() == "Wavealyze":
+        currentPath = "Wavealyze"
+    else:
+        currentPath = os.path.basename(mainWindow.windowTitle())
 
-    if not data.empty:
-        be = mainWindow.getBackEnd()
-        if mainWindow.windowTitle() == "Wavealyze":
-            currentPath = "Wavealyze"
-        else:
-            currentPath = os.path.basename(mainWindow.windowTitle())
+    msgBox = qtw.QMessageBox(mainWindow)
+    msgBox.setText("The document has been modified.")
+    msgBox.setInformativeText("Do you want to save your changes?")
+    msgBox.setWindowTitle("Save")
+    msgBox.setStandardButtons(qtw.QMessageBox.Yes | qtw.QMessageBox.No | qtw.QMessageBox.Cancel)
+    saveOption = msgBox.exec_()
 
-        msgBox = qtw.QMessageBox(mainWindow)
-        msgBox.setText("The document has been modified.")
-        msgBox.setInformativeText("Do you want to save your changes?")
-        msgBox.setWindowTitle("Save")
-        msgBox.setStandardButtons(qtw.QMessageBox.Yes | qtw.QMessageBox.No | qtw.QMessageBox.Cancel)
-        saveOption = msgBox.exec_()
+    if saveOption == qtw.QMessageBox.Yes:
+        options = qtw.QFileDialog.Options()
+        dialogFileName, ok = qtw.QFileDialog.getSaveFileName(mainWindow, "Select File Save Location",
+                                                             currentPath, "CSV file (*.csv)", options=options)
 
-        if saveOption == qtw.QMessageBox.Yes:
-            options = qtw.QFileDialog.Options()
-            dialogFileName, ok = qtw.QFileDialog.getSaveFileName(mainWindow, "Select File Save Location",
-                                                                 currentPath, "CSV file (*.csv)", options=options)
+        # If files are selected, send  these to the InputHandler
+        if ok:
+            mainWindow.setWindowTitle(dialogFileName)
+            exportCSV(mainWindow, dialogFileName)
+            be.setLastFileName(dialogFileName)
+            if dialogFileName not in be.getRecentFiles():
+                be.updateRecentFiles(dialogFileName)
+            be.clear()
 
-            # If files are selected, send  these to the InputHandler
-            if ok:
-                mainWindow.setWindowTitle(dialogFileName)
-                exportCSV(mainWindow, dialogFileName)
-                be.setLastFileName(dialogFileName)
-                if dialogFileName not in be.getRecentFiles():
-                    be.updateRecentFiles(dialogFileName)
-                be.clear()
-
-        elif saveOption == qtw.QMessageBox.No:
-            mainWindow.setWindowTitle("Wavealyze")
-            be.setLastFileName("Wavealyze")
-            mainWindow.tableView.backEnd.clear()
+    elif saveOption == qtw.QMessageBox.No:
+        mainWindow.setWindowTitle("Wavealyze")
+        be.setLastFileName("Wavealyze")
+        be.clear()
